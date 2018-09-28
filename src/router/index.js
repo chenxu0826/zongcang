c59e81b9345d6a44464435154dc4748c5aa9e69f
Trl <526220143@qq.com>
2018-09-28 15:08:53 +0800

    删除了无用的组件

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,11 @@ import outwork from '@/components/outwork/outwork'
 import crimalcheck from '@/components/crimalcheck/crimalcheck'
 import toolcheck from '@/components/toolcheck/toolcheck'
 import batchOut from '@/components/batchOut/batchOut'
-import position from '@/components/position/position'
-import mutualsupervision from '@/components/mutualsupervision/mutualsupervision'
-import cardbind from '@/components/cardbind/cardbind'
-import cellAdjust from '@/components/cellAdjust/cellAdjust'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       // 首页展示
       path: '/',
       name: 'home',
@@ -43,30 +38,6 @@ export default new Router({
       path: '/batchOut',
       name: 'batchOut',
       component: batchOut
-    },
-    {
-      // 定位展示
-      path: '/position',
-      name: 'position',
-      component: position
-    },
-    {
-      // 互监组
-      path: '/mutualsupervision',
-      name: 'mutualsupervision',
-      component: mutualsupervision
-    },
-    {
-      // 卡绑定
-      path: '/cardbind',
-      name: 'cardbind',
-      component: cardbind
-    },
-    {
-      //监房调整
-      path: '/cellAdjust',
-      name: 'cellAdjust',
-      component: cellAdjust
     }
   ]
 })
--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,9 @@ Vue.use(vuex);
 import home_store from '../components/home_store.js'
 import navheader_store from '../components/navheader_store.js'
 import crimalcheck_store from '../components/crimalcheck/crimalcheck_store.js'
-import mutualsupervision_store from '../components/mutualsupervision/mutualsupervision_store.js'
 import batchOut_store from '../components/batchOut/batchOut_store.js'
 import toolcheck_store from '../components/toolcheck/toolcheck_store.js'
-import cardbind_store from '../components/cardbind/cardbind_store.js'
 import outwork_store from '../components/outwork/outwork_store.js';
-import cellAdjust_store from '../components/cellAdjust/cellAdjust_store.js'
 
 
 export default new vuex.Store({
@@ -25,12 +22,9 @@ export default new vuex.Store({
     home: home_store,
     navheader: navheader_store,
     crimalcheck: crimalcheck_store,
-    mutualsupervision: mutualsupervision_store,
     batchOut: batchOut_store,
     toolcheck: toolcheck_store,
-    cardbind: cardbind_store,
     outwork: outwork_store,
-    cellAdjust: cellAdjust_store
   },
   mutations: {
     setMapList(state, Payload) {
